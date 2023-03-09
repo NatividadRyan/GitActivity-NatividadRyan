4c9e9f9b0e27adc57e3850abce7f9111bebc7011
Natividad Ryan <ryannatividad176@gmail.com>
2023-03-08 22:34:26 -0800

    My Personal Info added

---
+++ Desktop/GitActivity-NatividadRyan/temp.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+
+def personal_details():
+    name = "Ryan M. Natividad"
+    address = "Libtangin, Gasan, Marinduque"
+    birthday = "February 4,2002"
+    print("Name: {}\nAddress: {}\nBirthday: {}".format(name, address, birthday))
+
+personal_details()
